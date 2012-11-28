bef783c3af5abe7233519712493ee534006e257f
dimitar(laptop) <gindio@gmail.com>
2012-11-28 16:38:28 -0500

    Prepare to merge monkey with dimitar

---
+++ PTProject/Controllers/UnitController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PTProject.Models;
+using PTProject.ViewHelpers;
+
+
+namespace PTProject.Controllers
+{
+    public class UnitController : Controller
+    {
+        //
+        // GET: /CaseStudy/
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        //
+        // GET: /CaseStudy/Details/5
+
+        public ActionResult Details(int id)
+        {
+           
+            
+            var unit = Unit.find(id);
+            var unit_helper = new UnitHelper(unit);
+
+            return View(unit_helper);
+        }
+
+        //
+        // GET: /CaseStudy/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /CaseStudy/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Unit unit = new Unit();
+                unit.type = collection["type"];
+                unit.title = collection["title"]; 
+                unit.usersId = Convert.ToInt32(collection["usersId"]);
+                unit.in_progress = Convert.ToBoolean(collection["in_progress"]);
+                unit.approved = Convert.ToBoolean(collection["approved"]);
+
+                Unit.insert(unit);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+        //
+        // GET: /CaseStudy/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /CaseStudy/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /CaseStudy/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /CaseStudy/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+ 
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PTProject/ViewHelpers/ResultsHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PTProject.Models;
+using System.Web.Mvc;
+using System.Text;
+
+namespace PTProject.ViewHelpers
+{
+    public class ResultsHelper
+    {
+        public List<Unit> unique_units;
+        private List<Searchable> _searchables;
+
+        public ResultsHelper(List<Searchable> searchables)
+        {
+            this._searchables = searchables;
+            set_unique_units();
+        }
+
+        public MvcHtmlString print()
+        {
+            MvcHtmlString resulting_string = new MvcHtmlString("");
+
+           foreach( Searchable el in _searchables)
+           {
+               
+               var a_result_div = new TagBuilder("div");
+               var link = new TagBuilder("a");
+               var short_content = new TagBuilder("div");
+               var bold_s_term = new TagBuilder("b");
+
+               bold_s_term.InnerHtml = el.search_term;
+
+               link.Attributes["href"] = el.link;
+               link.SetInnerText(new StringBuilder(el.Unit.title).Append(": ").Append(el.pretty_type()).ToString());
+
+               short_content.InnerHtml =  el.short_content().Replace(el.search_term, bold_s_term.ToString());
+
+               a_result_div.InnerHtml = new StringBuilder(link.ToString()).Append(short_content.ToString()).ToString();
+
+               resulting_string = new MvcHtmlString(a_result_div.ToString());
+           }
+
+           return resulting_string;
+
+        }
+
+
+
+        private void set_unique_units()
+        {
+            List<int> unit_ids = new List<int>();
+            unique_units = new List<Unit>();
+
+            _searchables.ForEach(s =>
+                {
+
+                    if (!unit_ids.Contains(s.unitId))
+                    {
+                        
+                        unit_ids.Add(s.unitId);
+                        unique_units.Add(s.Unit);
+                    }
+
+                });
+        }
+    }
+}
